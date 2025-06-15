f22f2abfa805c4058031820aacccb3e082c9db50
Jeremy Stark <razestudiosdev@gmail.com>
2025-06-15 00:54:09 -0400

    Fixed color to output a char struct

--- src/color.hpp
+++ src/color.hpp
@@ -1,25 +1,14 @@
 #pragma once
-
 #include <glm/glm.hpp>
 #include <iostream>
 
 using color = glm::vec3;
 
+struct RGB {
+    unsigned char r, g, b;
+};
+
 auto write_color_ppm(std::ostream& out, const color& pixel_color) -> void {
-    auto r = pixel_color.x;
-    auto g = pixel_color.y;
-    auto b = pixel_color.z;
-
-    // Translate the [0,1] range to [0,255]
-    int rbyte = int(255 * r);
-    int gbyte = int(255 * g);
-    int bbyte = int(255 * b);
-
-    // Write out the pixel color components
-    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
-}
-
-auto write_color(std::ostream& out, const color& pixel_color) -> color {
     auto r = pixel_color.x;
     auto g = pixel_color.y;
     auto b = pixel_color.z;
@@ -29,7 +18,26 @@ auto write_color(std::ostream& out, const color& pixel_color) -> color {
     int gbyte = int(255.999 * g);
     int bbyte = int(255.999 * b);
 
+    // Write out the pixel color components
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+auto write_color(std::ostream& out, const color& pixel_color) -> RGB {
+    auto r = pixel_color.x;
+    auto g = pixel_color.y;
+    auto b = pixel_color.z;
+
+    // Translate the [0,1] range to [0,255]
+    int rbyte = int(255 * r);
+    int gbyte = int(255 * g);
+    int bbyte = int(255 * b);
+
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 
-    return color{rbyte, gbyte, bbyte};
+    RGB data;
+    data.r = static_cast<unsigned char>(rbyte);
+    data.g = static_cast<unsigned char>(gbyte);
+    data.b = static_cast<unsigned char>(bbyte);
+
+    return data;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,25 @@
 
 int main() {
 
-    // Image
-    const int image_width = 256;
-    const int image_height = 256;
+    auto aspect_ratio = 16.0/9.0;
+    int image_width = 400;
 
-    glm::vec3 data[image_width][image_height];
+    // Calculate the image height, and ensure that it's at least 1/
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Viewport widths less than one are ok since they are real valued.
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width)/image_height);
+
+    RGB data[image_width][image_height];
 
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << '\n' << std::flush;
         for (int i = 0; i < image_width; i++) {
             auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
-            
             data[i][j] = write_color(std::cout, pixel_color);
         }
     }
